b980a527f2112eaafb736c644233034430f48af6
John <darkneter@gmail.com>
2019-07-21 08:12:12 -0700

    fixed some bugs.

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
-//import * as sapper from '@sapper/app';
+import * as sapper from '@sapper/app';
 import './client/index.js';
-//sapper.start({
-//	target: document.querySelector('#sapper')
-//});
+sapper.start({
+	target: document.querySelector('#sapper')
+});
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,15 @@
+<svelte:head>
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/fontawesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/solid.min.css" >
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/brands.min.css" >
+	<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/gun/gun.js"></script>
+	<link href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css" />
+	<script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+</svelte:head>
+
 <!--
 <script>
 	import Nav from '../components/Nav.svelte';
